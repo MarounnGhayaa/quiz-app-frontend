58e99dbd447eae8d9c6ee1853b8226e7f30ad3c3
MarounnGhayaa <marounghaya@icloud.com>
2025-04-19 19:28:21 +0300

    Creating home page with its styling and functionality

---
+++ scripts/home.js
@@ -0,0 +1,34 @@
+const quizzes = [
+    {
+      id: 1,
+      title: "General Knowledge",
+      image: "../images/quiz1.jpg",
+    },
+    {
+      id: 2,
+      title: "JavaScript Basics",
+      image: "../images/quiz2.jpg",
+    },
+    {
+      id: 3,
+      title: "World-Wars History",
+      image: "../images/quiz3.jpg",
+    }
+  ];
+
+const quizzesList = document.getElementById("q-list");
+
+for (let i = 0; i < quizzes.length; i++) {
+    const quiz = quizzes[i];
+  
+    quizzesList.innerHTML +=`<div class="quiz">
+                                <div class="quiz-img">
+                                    <img src="${quiz.image}" alt="${quiz.title}" />
+                                </div>
+                                <div class="divider"></div>
+                                <div class="quiz-details">
+                                  <p><h3>${quiz.title}</h3></p>
+                                </div>
+                                <button class="start-quiz">Start Quiz</button> 
+                            </div>`;
+  }
---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+const users = [
+    { role: "admin", name: "Maroun", email: "admin@quiz.com", password: "admin123"},
+    { role: "user", name: "Nabiha", email: "nabiha@quiz.com", password: "nabiha123"}
+  ];
