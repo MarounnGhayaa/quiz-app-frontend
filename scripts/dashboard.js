510896cdc6bbc97e6f64f487a0f9a259faa33aa6
MarounnGhayaa <marounghaya@icloud.com>
2025-04-20 23:55:16 +0300

    Displaying some of their attributes

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,2 +1,11 @@
 const userList = document.getElementById('scores-list');
 const storedUsers = JSON.parse(localStorage.getItem("users"));
+
+for (let i = 0; i < storedUsers.length; i++) {
+    const user = storedUsers[i];
+    userList.innerHTML += `<div class="user">
+                           <h3>${user.name}</h3>
+                           <p>Email: ${user.email}</p>
+                           <p>Score: ${user.score}</p>
+                           </div>`;
+}
