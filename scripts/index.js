f1397cdbbedec92ad5c928b8440e368033a8393a
MarounnGhayaa <marounghaya@icloud.com>
2025-04-20 13:17:24 +0300

    Redirecting users logging in according to their roles (users/admins)

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+const users = [ 
+    { role: "admin", name: "Maroun", email: "admin@quiz.com", password: "admin123"},
+    { role: "user", name: "Nabiha", email: "nabiha@quiz.com", password: "nabiha123"}
+  ];
+
+localStorage.setItem("users", JSON.stringify(users));
+
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+  
+    const logMail = document.getElementById("log-email").value;
+    const logPass = document.getElementById("log-pass").value;
+  
+    const storedUsers = JSON.parse(localStorage.getItem("users"));
+  
+    let logger = null;
+    for (const user of storedUsers) {
+        if (user.email === logMail && user.password === logPass) {
+            logger = user;
+            break;
+        }
+    }
+  
+    if (logger) {
+      alert(`Welcome, ${logger.name}!`);
+        if (logger.role === "admin") {
+            window.location.href = ".pages/dashboard.html";
+        } else {
+            window.location.href = "./pages/home.html";            
+        }
+    } else {
+      alert("Invalid email or password. Please try again.");
+    }
+  });
