c117d1ee3ab711d617ad057d5119be464d0a765e
MarounnGhayaa <marounghaya@icloud.com>
2025-04-20 20:34:19 +0300

    Ading a div in quiz page and displaying the selected quiz

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,1 +1,15 @@
 const quizData = JSON.parse(localStorage.getItem("selectedQuiz"));
+const quizContainer = document.getElementById("quiz-container");
+
+for (let i = 0; i < quizData.questions.length; i++) {
+    const question = quizData.questions[i];
+    quizContainer.innerHTML += `<div class="question">
+                                <h3>${i + 1}. ${question.question}</h3>
+                                <div class="options">`;
+  
+    for (let j = 0; j < question.options.length; j++) {
+      quizContainer.innerHTML += `<button class="option-btn">${question.options[j]}</button>`;
+    }
+
+    quizContainer.innerHTML += `</div></div>`;
+  }
