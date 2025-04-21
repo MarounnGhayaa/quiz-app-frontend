3ee91cee97598a190fcddef71e1be3364d9665be
MarounnGhayaa <marounghaya@icloud.com>
2025-04-21 15:33:31 +0300

    Displaying a message when no users registered yet

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,17 +1,24 @@
 const userList = document.getElementById('scores-list');
 const storedUsers = JSON.parse(localStorage.getItem("users")) || [];
 
-for (let i = 0; i < storedUsers.length; i++) {
-    const user = storedUsers[i];
-    if (user.role === "admin") {
-        continue;
-    }
-    else {
-    userList.innerHTML += `<div class="user">
-                           <h3>${user.name}</h3>
-                           <p>Email: ${user.email}</p>
-                           <p>Score: ${user.score}</p>
-                           </div>`;
+if (storedUsers.length === 1) {
+    userList.innerHTML = `<div class="user">
+                          <p>There are no users registered yet!</p>
+                          </div>`;
+} 
+else {
+    for (let i = 0; i < storedUsers.length; i++) {
+        const user = storedUsers[i];
+        if (user.role === "admin") {
+            continue;
+        }
+        else {
+        userList.innerHTML += `<div class="user">
+                               <h3>${user.name}</h3>
+                               <p>Email: ${user.email}</p>
+                               <p>Score: ${user.score}</p>
+                               </div>`;
+        }
     }
 }
 
