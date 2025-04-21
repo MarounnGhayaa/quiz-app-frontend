0ed294f6cc5d394595d29463f19b41ec55807b8e
MarounnGhayaa <marounghaya@icloud.com>
2025-04-21 15:11:49 +0300

    Removing the user logged out from local storage

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -16,6 +16,7 @@ for (let i = 0; i < storedUsers.length; i++) {
 }
 
 document.getElementById("logoutBtn").addEventListener("click", function() {
+    localStorage.removeItem("currentUser");
     alert("You have logged out successfully!");
 
     window.location.href = "../index.html";
