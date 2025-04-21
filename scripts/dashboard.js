e00c4ada8294785fe4655dec76366657f70c5161
MarounnGhayaa <marounghaya@icloud.com>
2025-04-21 11:32:02 +0300

    Adding constraints to skip displaying the admin user

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -3,9 +3,14 @@ const storedUsers = JSON.parse(localStorage.getItem("users"));
 
 for (let i = 0; i < storedUsers.length; i++) {
     const user = storedUsers[i];
+    if (user.role === "admin") {
+        continue;
+    }
+    else {
     userList.innerHTML += `<div class="user">
                            <h3>${user.name}</h3>
                            <p>Email: ${user.email}</p>
                            <p>Score: ${user.score}</p>
                            </div>`;
+    }
 }
