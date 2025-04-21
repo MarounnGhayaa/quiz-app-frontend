e409d57611f5fec2c2f758ab36452beed6923af2
MarounnGhayaa <marounghaya@icloud.com>
2025-04-21 17:10:07 +0300

    Removing users not admin from the array

--- scripts/home.js
+++ scripts/home.js
@@ -1,7 +1,6 @@
 const users = [
-    { role: "admin", name: "Maroun", email: "admin@quiz.com", password: "admin123"},
-    { role: "user", name: "Nabiha", email: "nabiha@quiz.com", password: "nabiha123"}
-  ];
+  { role: "admin", name: "Maroun", email: "admin@quiz.com", password: "admin123"}
+];
   
 const quizzes = [
     {
